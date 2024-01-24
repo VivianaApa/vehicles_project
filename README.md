0a2518feaa18de4d172aa9da5ddf7ddc320807dd
VivianaApa <viviana.apariciob@gmail.com>
2024-01-24 16:33:31 -0600

    add notes file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# First-project
+#Vehicles Project
+#Esta aplicación web esta dedicada a la visualización de datos de anuncios de venta de automóviles. Esta plataforma ha sido diseñada para proporcionar a los usuarios una interfaz interactiva y fácil de usar que les permite explorar y comprender mejor la información contenida en nuestra base de datos de anuncios de automóviles
+
+#Funcionalidades principales: Visualizar graficos interactivos
--- app.py
+++ app.py
@@ -4,27 +4,35 @@ import plotly_express as px
 
 car_data = pd.read_csv('vehicles.csv') 
 
-st.header('C:\Users\vivia\vehicles_project\vehicles_env\notebooks\vehicles.csv')
-hist_button = st.button('./Construir histograma')
+st.header('Anuncios de venta de autos :red_car:', divider='blue')
 
-if hist_button: # al hacer clic en el botón hist
+st.title('Tabla de datos')
+tabla = st.dataframe(car_data.sample(20))
+new_filter = st.checkbox('new')
+if new_filter:
+        st.write('Visualizar una muestra solo de los autos nuevos')
+
+        tabla['condition'[tabla['condition']== 'new']]
+
+
+build_histogram = st.checkbox('Histograma')
+if build_histogram: # al hacer clic en el botón hist
         # escribir un mensaje
-        st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+        st.write('Construir un histograma para la columna odómetro')
             
-        # crear un histograma
+        # crear un histograma del kilometraje de los autos anunciados
         hist = px.histogram(car_data, x="odometer")
         
         # mostrar un gráfico Plotly interactivo
         st.plotly_chart(hist, use_container_width=True)
 
-scatter_button = st.button('Construir diagrama de dispersión')
-
-if scatter_button: # al hacer clic en el botón scatter
+build_scatter = st.checkbox('Diagrama de dispersión')
+if build_scatter: # al hacer clic en el botón scatter
         #escribir un mensaje
-        st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de choches')
+        st.write('Construir un diagrama de dispersión que relacione la distancia recorrida (odómetro), con el precio del auto')
 
         # crear un diagrama de dispersión 
         scat = px.scatter(car_data, x="odometer", y="price")
 
         # mostrar un gráfico Plotly interactivo
-        st.plotly_charts(scat)
+        st.plotly_chart(scat, use_container_width=True)
