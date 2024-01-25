7eeb447ef7ef0473763d0d939bc971487ded1860
VivianaApa <viviana.apariciob@gmail.com>
2024-01-24 23:16:37 -0600

    Añade nuevas funcionalidades de visualización de gráficos

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 #Vehicles Project
-#Esta aplicación web esta dedicada a la visualización de datos de anuncios de venta de automóviles. Esta plataforma ha sido diseñada para proporcionar a los usuarios una interfaz interactiva y fácil de usar que les permite explorar y comprender mejor la información contenida en nuestra base de datos de anuncios de automóviles
+#Esta aplicación web esta dedicada a la visualización de datos de anuncios de venta de automóviles. Esta plataforma ha sido diseñada para proporcionar a los usuarios una interfaz interactiva y fácil de usar que les permite explorar y comprender mejor la información contenida en nuestra base de datos de anuncios de automóviles en venta.
 
-#Funcionalidades principales: Visualizar graficos interactivos
+#Funcionalidades principales: 
+#Visualizar graficos interactivos que contienen información relevante sobre aspectos clave de los autos anunciados
+#Funciones para filtrar los datos que se muestran en los gráficos segun los intereses del usuario
--- app.py
+++ app.py
@@ -6,33 +6,75 @@ car_data = pd.read_csv('vehicles.csv')
 
 st.header('Anuncios de venta de autos :red_car:', divider='blue')
 
-st.title('Tabla de datos')
-tabla = st.dataframe(car_data.sample(20))
-new_filter = st.checkbox('new')
-if new_filter:
-        st.write('Visualizar una muestra solo de los autos nuevos')
+st.subheader('Tabla de muestra')
 
-        tabla['condition'[tabla['condition']== 'new']]
+#Casilla de verificación para mostrar solo autos nuevos en la tabla de muestra
+new_filter = st.checkbox('Mostrarme solo autos nuevos') 
 
+if new_filter: # al marcar la casilla new_filter
+        # mostrar el mensaje
+        st.write('Muestra ejemplo de 20 autos nuevos')
 
-build_histogram = st.checkbox('Histograma')
-if build_histogram: # al hacer clic en el botón hist
-        # escribir un mensaje
-        st.write('Construir un histograma para la columna odómetro')
+        # y filtrar la muestra del dataframe para visualizar solo autos nuevos
+        st.dataframe(car_data[car_data['condition'] == 'new'].sample(20))
+else: 
+        st.dataframe(car_data.sample(20))
+
+st.subheader('Marcas de autos en venta')
+
+#Crear una columna que muestre solo la marca del auto, extraida de la columna model
+car_data['company'] = car_data['model'].str.split().str[0] 
+
+#Gráfico de barras que muestra la cantidad de autos por marca
+company_bar = px.bar(car_data['company'].value_counts(), title= 'Cantidad de autos en venta por Marca')
+st.plotly_chart(company_bar, use_container_width=True)
+
+st.subheader('Millas recorridas de los autos en venta')
+#Crear una caja desplegable
+chart_type = st.selectbox("Selecciona el tipo de gráfico", ["None", "Histograma", "Diagrama de Dispersión"])
+
+if chart_type == "Histograma": # al seleccionar Histograma en el selectbox
+        # mostrar el mensaje
+        st.write('Histograma para la columna odómetro')
             
         # crear un histograma del kilometraje de los autos anunciados
-        hist = px.histogram(car_data, x="odometer")
+        hist = px.histogram(car_data, x="odometer", title='Cantidad de millas recorridas por los autos en venta')
         
-        # mostrar un gráfico Plotly interactivo
+        # mostrar el gráfico Plotly interactivo
         st.plotly_chart(hist, use_container_width=True)
 
-build_scatter = st.checkbox('Diagrama de dispersión')
-if build_scatter: # al hacer clic en el botón scatter
-        #escribir un mensaje
-        st.write('Construir un diagrama de dispersión que relacione la distancia recorrida (odómetro), con el precio del auto')
+elif chart_type == "Diagrama de Dispersión": # al hacer clic en el botón scatter
+        #mostrar el mensaje
+        st.write('Diagrama de dispersión Precio vs Odómetro')
 
         # crear un diagrama de dispersión 
-        scat = px.scatter(car_data, x="odometer", y="price")
+        odom_price = px.scatter(car_data, x="odometer", y="price", title='Relacion entre las millas recorridas y el precio de los autos en venta')
 
-        # mostrar un gráfico Plotly interactivo
-        st.plotly_chart(scat, use_container_width=True)
+        # mostrar el gráfico Plotly interactivo
+        st.plotly_chart(odom_price, use_container_width=True)
+else:
+        st.write('Aún no has seleccionado')
+
+st.subheader('Precio de los autos en venta')
+
+#Crear un boton 
+price_cond_button = st.button('Comparar Precio vs Condicion')
+
+if price_cond_button: # al hacer clic en price_cond_button
+        # mostrar el mensaje
+        st.write('Histograma que relaciona los precios de los autos en venta de acuerdo a su condición. Selecciona la condicion de tu interés')
+
+        # crear un histograma de los precios segun la condición del auto
+        price_cond = px.histogram(car_data, x= "price", color= "condition", title= 'Frecuencia de precios segun la condición del auto en venta')
+        st.plotly_chart(price_cond, use_container_width=True)
+
+#Crear un boton 
+price_comp_button = st.button('Comparar Precios entre Marcas')
+
+if price_comp_button: #Al hacer clic en el boton price_comp_button
+        # mostrar el mensaje
+        st.write('Histograma que relaciona los precios de los autos en venta de acuerdo a la marca. Selecciona la marca de tu interés')
+        # crear un histograma de los precios segun la marca del auto
+        price_comp = px.histogram(car_data, x= "price", color= "company", title= 'Frecuencia de precios segun la marca de los autos en venta')
+        # mostrar el plotly interactivo
+        st.plotly_chart(price_comp, use_container_width=True)
